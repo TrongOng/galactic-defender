2190acd07662ed1a99358e41e1dff5466f932255
Trong Ong <tvong@student.ysu.edu>
2024-01-06 04:13:10 -0500

    change speed

--- settings.py
+++ settings.py
@@ -11,8 +11,8 @@ class Settings:
         # Ship settings
         self.ship_speed = 1.1
 
-        self.bullet_speed = 5.0
+        self.bullet_speed = 2.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (255, 60, 60)
-        self.bullets_allowed = 10.0
+        self.bullets_allowed = 1.0
